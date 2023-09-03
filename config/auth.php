aee3e3109c8e18ed637decdc0fbbfc2e79657248
Danilo Santos <danilondosantos@gmail.com>
2023-09-03 13:41:12 -0300

    feat: Adicionando configuração para o Guard

--- config/auth.php
+++ config/auth.php
@@ -17,6 +17,18 @@ return [
         'guard' => 'web',
         'passwords' => 'users',
     ],
+    'admin' => [
+        'driver' => 'eloquent',
+        'model'  => App\Models\Admin::class,
+    ],
+    'seller' => [
+        'driver' => 'eloquent',
+        'model'  => App\Models\Seller::class,
+    ],
+    'client' => [
+        'driver' => 'eloquent',
+        'model'  => App\Models\Client::class,
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -39,6 +51,18 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+        'seller' => [
+            'driver' => 'session',
+            'provider' => 'sellers',
+        ],
+        'client' => [
+            'driver' => 'session',
+            'provider' => 'clients',
         ],
     ],
 
@@ -63,6 +87,18 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
+        'sellers' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Seller::class,
+        ],
+        'clients' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Client::class,
         ],
 
         // 'users' => [
@@ -96,6 +132,18 @@ return [
             'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class
+        ],
+        'sellers' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Seller::class
+        ],
+        'clients' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Client::class
         ],
     ],
 
