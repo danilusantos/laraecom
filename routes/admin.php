14c68187fae71b171cf7a6d45c4579417e2338bd
Danilo Santos <danilondosantos@gmail.com>
2023-09-05 15:52:12 -0300

    feat: Prevent Back History

--- routes/admin.php
+++ routes/admin.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AdminController;
 
 Route::prefix('admin')->name("admin.")->group(function(){
-    Route::middleware(['guest:admin'])->group(function(){
+    Route::middleware(['guest:admin', 'PreventBackHistory'])->group(function(){
         Route::view('/login', 'admin.pages.admin.auth.login')->name('login');
         Route::post('/login_handler', [AdminController::class, 'loginHandler'])->name('login_handler');
     });
 
-    Route::middleware(['auth:admin'])->group(function(){
+    Route::middleware(['auth:admin', 'PreventBackHistory'])->group(function(){
         Route::view('/home', 'admin.pages.admin.home')->name('home');
         Route::post('/logout_handler', [AdminController::class, 'logoutHandler'])->name('logout_handler');
     });
